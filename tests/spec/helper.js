b9875575c844b2c29406a81d58e6acdce7aa00e0
Sam Schurter <skippotter1@gmail.com>
2020-11-18 13:35:27 -0600

    upgrades to jasmine 3.6

--- tests/spec/helper.js
+++ tests/spec/helper.js
@@ -1,5 +1,45 @@
 jasmine.getFixtures().fixturesPath = 'http://localhost:9001/tests/spec';
 
+// var fixturesCache = {};
+// var containerId = 'jasmine-fixtures';
+// function loadFixtures() {
+//     //should save and restore the body element, not just the container
+//     var container = document.getElementById(containerId);
+//     if (container) {
+//         container.parentNode.removeChild(container);
+//     }
+//     var htmlChunks = [];
+
+//     var fixtureUrls = arguments;
+//     for (var urlCount = fixtureUrls.length, urlIndex = 0; urlIndex < urlCount; urlIndex++) {
+//         url = fixtureUrls[urlIndex];
+//         if (void (0) === fixturesCache[url]) {
+//             var xhr = new window.XMLHttpRequest;
+//             xhr.open('GET', url, false);
+//             xhr.send(null);
+//             var status = xhr.status;
+//             var succeeded = 0 === status || (status >= 200 && status < 300) || 304 == status;
+
+//             if (!succeeded)
+//                 throw new Error('Failed to load resource: status=' + status + ' url=' + url);
+//             fixturesCache[url] = xhr.responseText;
+//         }
+//         htmlChunks.push(fixturesCache[url]);
+//     }
+//     html = htmlChunks.join('');
+//     var container = document.createElement('div');
+//     container.id = this.containerId;
+
+//     if (html && html.nodeType === 1)
+//         container.appendChild(html);
+//     else
+//         container.innerHTML = html;
+
+//     document.body.appendChild(container);
+
+// }
+
+
 beforeEach(function() {
 
   /**
