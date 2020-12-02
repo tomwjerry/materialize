015c991f32dd5cff248f5431efa57da8d0799eff
Tom Elssjö <tomelssjo@gmail.com>
2020-12-02 11:21:52 +0100

    Fixed card spec test

--- tests/spec/cards/cardsSpec.js
+++ tests/spec/cards/cardsSpec.js
@@ -1,6 +1,4 @@
 describe( "Cards", function () {
-  var reveal;
-
   beforeEach(async function() {
     await XloadFixtures(['cards/cardsFixture.html']);
   });
@@ -13,31 +11,34 @@ describe( "Cards", function () {
     var revealCard;
 
     beforeEach(function() {
-      revealCard = $('.card.reveal');
+      revealCard = document.querySelector('.card.reveal');
     });
 
     it("should have a hidden card-reveal", function (done) {
-      var revealDiv = revealCard.find('.card-reveal');
-      var activator = revealCard.find('.activator');
+      var revealDiv = revealCard.querySelector('.card-reveal');
+      var activator = revealCard.querySelector('.activator');
+      var computedDisplay = getComputedStyle(revealDiv);
 
-      expect(revealDiv).toBeHidden('reveal div should be hidden initially'); //TODO replace with alternative for deprecated jasmine-jquery
+      expect(computedDisplay.getPropertyValue('display')).toEqual('none', 'reveal div should be hidden initially'); //TODO replace with alternative for deprecated jasmine-jquery
 
       setTimeout(function() {
         activator.click();
 
         setTimeout(function() {
-          expect(revealDiv).toBeVisible('reveal did not appear after activator was clicked.'); //TODO replace with alternative for deprecated jasmine-jquery
+          computedDisplay = getComputedStyle(revealDiv);
+          expect(computedDisplay.getPropertyValue('display')).toEqual('block', 'reveal did not appear after activator was clicked.'); //TODO replace with alternative for deprecated jasmine-jquery
 
           // Check revealDiv covers reveal card.
-          expect(revealDiv.outerWidth()).toEqual(revealCard.outerWidth(), 'activator was not as wide as reveal card.');
-          expect(revealDiv.outerHeight()).toEqual(revealCard.outerHeight(), 'activator was not as high as reveal card.');
-          expect(revealDiv.offset().top).toEqual(revealCard.offset().top, 'activator was not as in the same y as reveal card.');
-          expect(revealDiv.offset().left).toEqual(revealCard.offset().left, 'activator was not as in the same x as reveal card.');
+          var revealDivPositions = revealDiv.getBoundingClientRect();
+          var revealCardPositions = revealCard.getBoundingClientRect();
+          expect(revealDivPositions.width).toEqual(revealCardPositions.width, 'activator was not as wide as reveal card.');
+          expect(revealDivPositions.height).toEqual(revealCardPositions.height, 'activator was not as high as reveal card.');
+          expect(revealDivPositions.top).toEqual(revealCardPositions.top, 'activator was not as in the same y as reveal card.');
+          expect(revealDivPositions.left).toEqual(revealCardPositions.left, 'activator was not as in the same x as reveal card.');
 
           done();
         }, 400);
       }, 400);
-
     });
   });
 
@@ -45,14 +46,16 @@ describe( "Cards", function () {
     var imageCard;
 
     beforeEach(function() {
-      imageCard = $('.card.image');
+      imageCard = document.querySelector('.card.image');
     });
 
     it("should have an image that fills to full width of card", function () {
-      var image = imageCard.find('.card-image > img');
+      var image = imageCard.querySelector('.card-image > img');
+      var imagePositions = image.getBoundingClientRect();
+      var imageCardPositions = imageCard.getBoundingClientRect();
 
-      expect(image.outerWidth()).toEqual(imageCard.outerWidth(), 'image does not fill width of card');
-      expect(image.offset().top).toEqual(imageCard.offset().top, 'image was not as in the same y as card.');
+      expect(imagePositions.width).toEqual(imageCardPositions.width, 'image does not fill width of card');
+      expect(imagePositions.top).toEqual(imageCardPositions.top, 'image was not as in the same y as card.');
     });
   });
 
@@ -61,46 +64,57 @@ describe( "Cards", function () {
     var small, medium, large;
 
     beforeEach(function() {
-      small = $('.card.small');
-      medium = $('.card.medium');
-      large = $('.card.large');
+      small = document.querySelector('.card.small');
+      medium = document.querySelector('.card.medium');
+      large = document.querySelector('.card.large');
     });
 
     it("should have small card dimensions", function () {
-      var cardImage = small.find('.card-image');
-      var cardContent = small.find('.card-content');
-      var cardAction = small.find('.card-action');
+      var cardImage = small.querySelector('.card-image');
+      var cardContent = small.querySelector('.card-content');
+      var cardAction = small.querySelector('.card-action');
+      var smallRect = small.getBoundingClientRect();
+      var cardImageRect = cardImage.getBoundingClientRect();
+      var cardContentRect = cardContent.getBoundingClientRect();
+      var cardActionRect = cardAction.getBoundingClientRect();
 
-      expect(small.outerHeight()).toEqual(300, 'small card should be 300px high');
-      expect(cardImage.outerHeight()).toBeLessThan(181, 'small image should be <= 180px or 60% high');
-      expect(cardContent.outerHeight()).toBeLessThan(121, 'small content should be <= 120px or 40% high');
-      expect(cardAction.offset().top + cardAction.outerHeight())
-        .toEqual(small.offset().top + small.outerHeight(), 'small action should be at bottom of card');
+      expect(smallRect.height).toEqual(300, 'small card should be 300px high');
+      expect(cardImageRect.height).toBeLessThan(181, 'small image should be <= 180px or 60% high');
+      expect(cardContentRect.height).toBeLessThan(121, 'small content should be <= 120px or 40% high');
+      expect(cardActionRect.top + cardActionRect.height)
+        .toEqual(smallRect.top + smallRect.height, 'small action should be at bottom of card');
     });
 
     it("should have medium card dimensions", function () {
-      var cardImage = medium.find('.card-image');
-      var cardContent = medium.find('.card-content');
-      var cardAction = medium.find('.card-action');
+      var cardImage = medium.querySelector('.card-image');
+      var cardContent = medium.querySelector('.card-content');
+      var cardAction = medium.querySelector('.card-action');
+      var mediumRect = medium.getBoundingClientRect();
+      var cardImageRect = cardImage.getBoundingClientRect();
+      var cardContentRect = cardContent.getBoundingClientRect();
+      var cardActionRect = cardAction.getBoundingClientRect();
 
-      expect(medium.outerHeight()).toEqual(400, 'medium card should be 400px high');
-      expect(cardImage.outerHeight()).toBeLessThan(241, 'medium image should be <= 240 or 60% high');
-      expect(cardContent.outerHeight()).toBeLessThan(161, 'medium content should be <= 160px or 40% high');
-      expect(cardAction.offset().top + cardAction.outerHeight())
-        .toEqual(medium.offset().top + medium.outerHeight(), 'medium action should be at bottom of card');
+      expect(mediumRect.height).toEqual(400, 'medium card should be 400px high');
+      expect(cardImageRect.height).toBeLessThan(241, 'medium image should be <= 240 or 60% high');
+      expect(cardContentRect.height).toBeLessThan(161, 'medium content should be <= 160px or 40% high');
+      expect(cardActionRect.top + cardActionRect.height)
+        .toEqual(mediumRect.top + mediumRect.height, 'medium action should be at bottom of card');
     });
 
     it("should have large card dimensions", function () {
-      var cardImage = large.find('.card-image');
-      var cardContent = large.find('.card-content');
-      var cardAction = large.find('.card-action');
+      var cardImage = large.querySelector('.card-image');
+      var cardContent = large.querySelector('.card-content');
+      var cardAction = large.querySelector('.card-action');
+      var largeRect = large.getBoundingClientRect();
+      var cardImageRect = cardImage.getBoundingClientRect();
+      var cardContentRect = cardContent.getBoundingClientRect();
+      var cardActionRect = cardAction.getBoundingClientRect();
 
-      expect(large.outerHeight()).toEqual(500, 'large card should be 500px high');
-      expect(cardImage.outerHeight()).toBeLessThan(301, 'large image should be <= 300 or 60% high');
-      expect(cardContent.outerHeight()).toBeLessThan(201, 'large content should be <= 200 or 40% high');
-      expect(cardAction.offset().top + cardAction.outerHeight())
-        .toEqual(large.offset().top + large.outerHeight(), 'large action should be at bottom of card');
+      expect(largeRect.height).toEqual(500, 'large card should be 500px high');
+      expect(cardImageRect.height).toBeLessThan(301, 'large image should be <= 300 or 60% high');
+      expect(cardContentRect.height).toBeLessThan(201, 'large content should be <= 200 or 40% high');
+      expect(cardActionRect.top + cardActionRect.height)
+        .toEqual(largeRect.top + largeRect.height, 'large action should be at bottom of card');
     });
   });
-
 });
