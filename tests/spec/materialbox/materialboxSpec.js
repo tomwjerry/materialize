0819db0e320b24cffbcdfb68fd70a45e36e1d760
Tom Elssjö <tomelssjo@gmail.com>
2020-12-03 21:30:32 +0100

    Materiabox test w/o jquery

--- tests/spec/materialbox/materialboxSpec.js
+++ tests/spec/materialbox/materialboxSpec.js
@@ -10,19 +10,20 @@ describe( 'Materialbox:', function() {
 
   describe('Materialbox opens correctly with transformed ancestor', function() {
     it('Opens a correctly placed overlay when clicked', function(done) {
-      transformMaterialbox = $('#transformTest');
-      $('.materialboxed').materialbox();
+      transformMaterialbox = document.querySelector('#transformTest');
+      M.Materialbox.init(document.querySelector('.materialboxed'));
 
       // Mouse click
-      transformMaterialbox.find('.materialboxed').trigger('click');
+      click(transformMaterialbox.querySelector('.materialboxed'));
       setTimeout(function() {
         // Check overlay is attached
-        var overlay = transformMaterialbox.find('#materialbox-overlay');
-        var overlayRect = overlay[0].getBoundingClientRect();
+        var overlay = transformMaterialbox.querySelector('#materialbox-overlay');
+        var overlayRect = overlay.getBoundingClientRect();
         var windowWidth = window.innerWidth;
         var windowHeight = window.innerHeight;
-        expect(overlay).toExist('because it is generated on init'); //TODO replace with alternative for deprecated jasmine-jquery
-        expect(overlay).toBeVisible('because materialbox was clicked'); //TODO replace with alternative for deprecated jasmine-jquery
+        var overlayStyle = getComputedStyle(overlay);
+        expect(overlay).toBeTruthy('because it is generated on init'); //TODO replace with alternative for deprecated jasmine-jquery
+        expect(overlayStyle.getPropertyValue('display')).toEqual('block', 'because materialbox was clicked'); //TODO replace with alternative for deprecated jasmine-jquery
         expect(overlayRect.top).toEqual(0);
         expect(overlayRect.left).toEqual(0);
         expect(overlayRect.width).toEqual(windowWidth);
